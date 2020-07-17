c757593257119b570ec5517857cadae3ae87a83f
ffranco4 <52253626+ffranco4@users.noreply.github.com>
2020-07-17 13:49:24 -0500

    single view app

--- calculator 2/ViewController.swift
+++ calculator 2/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentGradeTextField: UITextField!
+    
+    @IBOutlet weak var desiredGradeTextField: UITextField!
+    
+    @IBOutlet weak var percentageTextField: UITextField!
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        
+        let currentGrade = Double(currentGradeTextField.text!)
+        
+        let desiredGrade = Double(desiredGradeTextField.text!)
+        
+        let percentage = Double(percentageTextField.text!)
+        
+        let oneHundredPercent = 100.0
+        let percentOfGrade = Double(oneHundredPercent - percentage!)/100.0
+        
+        let gradeNeededOnFinal = (desiredGrade! - (percentOfGrade * currentGrade!))/percentage!
+        finalGradeLabel.text = "You need a \(gradeNeededOnFinal * 100.0)%"
+        
+    
+    }
+    
+    @IBOutlet weak var finalGradeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
